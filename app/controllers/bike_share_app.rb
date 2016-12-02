33011e12e6a53de688a884b20f1103c3ae71b607
Mike Schutte <tmikeschutte@gmail.com>
2016-12-01 17:40:06 -0700

    initial commit. adds controller

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,46 @@
+require_relative '../models/station'
+
 class BikeShareApp < Sinatra::Base
+  
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/' do
+    erb :dashboard
+  end
+  
+  get '/stations' do
+    @stations = Station.all
+    erb :stations_index
+  end
+
+  get '/stations/new' do
+    erb :new_station
+  end
+
+  post '/stations' do
+    Station.create(params[:station])
+    redirect '/stations'
+  end
+
+  get '/stations/:id' do
+    @station = Station.find(params[:id])
+    erb :show_station
+  end
+
+  get '/stations/:id/edit' do
+    @station = Station.find(params[:id])
+    erb :edit_station
+  end
+
+  put '/stations/:id' do
+    Station.update(params[:id], params[:station])
+    redirect "/stations/#{params[:id]}"
+  end
+
+  delete '/stations/:id' do
+    Station.destroy(params[:id])
+    redirect '/stations'
+  end
 
 end
