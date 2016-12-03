a5743eb7fa84af6de991f127e5073ad499763a03
Ethan Bennett <ebennett19@gmail.com>
2016-12-02 18:06:50 -0700

    stations table created

--- db/migrate/20161203004556_create_stations.rb
+++ db/migrate/20161203004556_create_stations.rb
@@ -1,10 +1,10 @@
 class CreateStations < ActiveRecord::Migration[5.0]
   def change
-    create_table :stations do |t|
-      t.string :name
-      t.integer :dock_count
-      t.integer :city_id
-      
+    create_table  :stations do |t|
+      t.string    :name
+      t.integer   :dock_count
+      t.integer   :city_id
+      t.datetime  :installation_date
       t.timestamps null: false
     end
   end
