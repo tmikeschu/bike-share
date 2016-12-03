179ecd5d1f8ef1b44ed488bb45b22aa2da8dfed5
Mike Schutte <tmikeschutte@gmail.com>
2016-12-03 08:15:51 -0700

    adds stations index view and tests

---
+++ spec/features/user_sees_index_of_stations.rb
@@ -0,0 +1,19 @@
+require_relative '../spec_helper'
+
+RSpec.describe "When a user visits '/stations'" do
+  it "shows them an all stations heading" do
+    visit('/stations')
+    within 'h1' do
+      expect(page).to have_text "All Stations"
+    end
+  end
+
+  it "shows a station's name" do
+    Station.create(name: "Denver", @dock_count)
+    visit('/stations')
+    within 'h1 nth-of-type(1)' do
+      expect(page).to have_text  
+    end
+  end
+
+end
