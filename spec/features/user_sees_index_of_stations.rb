750d83dd5bd2383e4ea6897081049ff2bfcf7882
Mike Schutte <tmikeschutte@gmail.com>
2016-12-03 09:57:15 -0700

    adds spacing to index test

--- spec/features/user_sees_index_of_stations.rb
+++ spec/features/user_sees_index_of_stations.rb
@@ -15,18 +15,23 @@ RSpec.describe "When a user visits '/stations'" do
     Station.create(name: "Fort Collins Harmony", dock_count: 9, city_id: 2, installation_date: "12/1/2013")
     visit('/stations')
     # save_and_open_page
+
     within 'ul:nth-of-type(1) > li:nth-of-type(1)' do
       expect(page).to have_text "Denver Union Station"
     end
+
     within 'ul:nth-of-type(1) > li:nth-of-type(2)' do
       expect(page).to have_text "7"
     end
+
     within 'ul:nth-of-type(1) > li:nth-of-type(3)' do
       expect(page).to have_text "Denver"
     end
+
     within 'ul:nth-of-type(1) > li:nth-of-type(4)' do
       expect(page).to have_text "2013-02-12"
     end
+    
   end
 
 end
