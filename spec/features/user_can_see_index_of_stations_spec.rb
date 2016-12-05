785978f9e6d8cf868f69774e3732e1d0d656f380
Mike Schutte <tmikeschutte@gmail.com>
2016-12-05 11:13:38 -0700

    update stations index feature test

--- spec/features/user_can_see_index_of_stations_spec.rb
+++ spec/features/user_can_see_index_of_stations_spec.rb
@@ -1,55 +1,66 @@
 require_relative '../spec_helper'
 
-describe 'Stations New Page' do
+describe 'When a user visists /stations' do
 
   before do
     City.create(name: "Denver")
     City.create(name: "Fort Collins")
-    @station1 = City.first.stations.create(name: "Denver Union Station", dock_count: 7, installation_date: "12/2/2013", lat: 37.330698, long: -121.888979)
-    @station2 = City.last.stations.create(name: "Fort Collins Harmony", dock_count: 9, city_id: 2, installation_date: "8/10/2013", lat: 39.330698, long: -120.888979)
+    @station1 = City.first.stations.create(name: "Denver Union Station", dock_count: 7, installation_date: "2013/12/02", lat: 37.330698, long: -121.888979)
+    @station2 = City.last.stations.create(name: "Fort Collins Harmony", dock_count: 9, city_id: 2, installation_date: "2013/8/10", lat: 39.330698, long: -120.888979)
     visit('/stations')
   end
 
-  describe "When a user visits '/stations'" do
-    it "they see an all stations heading" do
+  it 'there are map markers on the page' do
+    expect(page).to have_content "addMarker"
+  end
+    
+
+  describe "they can see" do
+    it "an all stations heading" do
       within '.all-stations' do
         expect(page).to have_text "All Stations"
       end
     end
 
-    it "they see a station's information" do
-      within 'div.station-summary:nth-of-type(1) > ul > li:nth-of-type(1)' do
+    it "a station's information" do
+      within 'div.station-summary:nth-of-type(1)' do
         expect(page).to have_content "Denver Union Station"
-      end
-
-      within 'div.station-summary:nth-of-type(1) > ul > li:nth-of-type(2)' do
         expect(page).to have_content 7
-      end
-
-      within 'div.station-summary:nth-of-type(1) > ul > li:nth-of-type(3)' do
         expect(page).to have_content "Denver"
-      end
-
-      within 'div.station-summary:nth-of-type(1) > ul > li:nth-of-type(4)' do
-        expect(page).to have_content "2013-02-12"
-      end
-
-      within 'div.station-summary:nth-of-type(1) > form:nth-of-type(1)' do
+        expect(page).to have_content 37.330698
+        expect(page).to have_content -121.888979
+        expect(page).to have_content "2013-12-02"
         expect(page).to have_button "New"
-      end
-      
-      within 'div.station-summary:nth-of-type(1) > form:nth-of-type(2)' do
         expect(page).to have_button "Edit"
+        expect(page).to have_button "Delete"        
       end
+    end
 
-      within 'div.station-summary:nth-of-type(1) > form:nth-of-type(3)' do
-        expect(page).to have_button "Delete"
+    it "and a different station's information" do
+      within 'div.station-summary:nth-of-type(2)' do
+        expect(page).to have_content "Fort Collins Harmony"
+        expect(page).to have_content 9
+        expect(page).to have_content "Fort Collins"
+        expect(page).to have_content 39.330698
+        expect(page).to have_content -120.888979
+        expect(page).to have_content "2013-08-10"
+        expect(page).to have_button "New"
+        expect(page).to have_button "Edit"
+        expect(page).to have_button "Delete"        
       end
-
     end
   end
 
-  describe 'when they click edit' do
+  describe 'and when they click new' do
+    it 'they are taken to the edit page' do
+      within 'div.station-summary:nth-of-type(1)' do
+        click_on "New"
+        expect(current_path).to eq("/stations/new")
+      end
+    end
+  end
+
+  describe 'and when they click edit' do
     it 'they are taken to the edit page' do
       within 'div.station-summary:nth-of-type(1)' do
         click_on "Edit"
@@ -58,7 +69,7 @@ describe 'Stations New Page' do
     end
   end
 
-  describe 'when they click delete' do
+  describe 'and when they click delete' do
     it 'they are taken to the index page' do
       within 'div.station-summary:nth-of-type(2)' do
         expect(page.body).to include("Fort Collins")
@@ -68,4 +79,5 @@ describe 'Stations New Page' do
       end
     end
   end
+
 end
