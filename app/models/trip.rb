8b419551086ab2f6bbca1929645526c4eadcce75
Caroline Powell <carpowell10@gmail.com>
2016-12-06 10:44:45 -0700

    Add aliases for end and start stations and trips.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -10,7 +10,8 @@ class Trip < ActiveRecord::Base
             :user_zip_code,
             presence: true
 
-  belongs_to :station, :foreign_key => :id
+  belongs_to :start_station, class_name: 'Station', :foreign_key => :start_station_id
+  belongs_to :end_station, class_name: 'Station', :foreign_key => :end_station_id
   belongs_to :subscription_type
   belongs_to :weather_condition, :foreign_key => :start_date
 
