7959cedcb65690462f48805f69af632e34e865a5
Mike Schutte <tmikeschutte@gmail.com>
2016-12-06 16:47:29 -0700

    complete conditions analytics methods

--- app/models/weather_condition.rb
+++ app/models/weather_condition.rb
@@ -33,27 +33,86 @@ class WeatherCondition < ActiveRecord::Base
   end
 
   def self.days_with_visibility(miles)
-    WeatherCondition.where(mean_visibility_miles: [speed..speed + 3])
+    WeatherCondition.where(mean_visibility_miles: [miles..miles + 3])
+  end
+
+  def self.temperature_metrics(degrees)
+    range = self.metric_range_with_increments_of(:max_temperature_f, degrees)
+    range.reduce({}) do |result, degrees|
+      days = self.days_with_high_temp(degrees)
+      trips = self.trips_on_days(days).values
+      result["#{degrees}"] = self.ride_metrics(trips)
+      result
+    end
+  end
+
+  def self.precipitation_metrics(inches)
+    range = self.metric_range_with_increments_of(:precipitation_inches, inches)
+    range.reduce({}) do |result, inches|
+      days = self.days_with_precip_inches(inches)
+      trips = self.trips_on_days(days).values
+      result["#{inches}"] = self.ride_metrics(trips)
+      result
+    end
+  end
+
+  def self.wind_metrics(speed)
+    range = self.metric_range_with_increments_of(:mean_wind_speed_mph, speed)
+    range.reduce({}) do |result, speed|
+      days = self.days_with_wind_speed(speed)
+      trips = self.trips_on_days(days).values
+      result["#{speed}"] = self.ride_metrics(trips)
+      result
+    end
+  end
+
+  def self.visibility_metrics(miles)
+    range = self.metric_range_with_increments_of(:mean_visibility_miles, miles)
+    range.reduce({}) do |result, miles|
+      days = self.days_with_visibility(miles)
+      trips = self.trips_on_days(days).values
+      result["#{miles}"] = self.ride_metrics(trips)
+      result
+    end
+  end
+
+  def self.master_metrics
+    metrics = {}
+    degrees, inches, speed, miles   = 10, 0.5, 4, 4
+    metrics[:temperature]       = self.temperature_metrics(degrees)
+    metrics[:precipitation]  = self.precipitation_metrics(inches)
+    metrics[:wind]   = self.wind_metrics(speed)
+    metrics[:visibility] = self.visibility_metrics(miles)
+    metrics
   end
 
   def self.average_rides(rides)
-    rides.reduce(:+) / rides.count
+    return 0 if rides.count.zero?
+    rides.reduce(:+).to_i / rides.count 
   end
 
   def self.highest_rides(rides)
-    rides.sort[-1]
+    rides.sort[-1].to_i
   end
 
   def self.lowest_rides(rides)
-    rides.sort[0]
+    rides.sort[0].to_i
   end
 
-  def self.temp_range_with_increments_of(incrementer)
-    lower = WeatherCondition.minimum(:max_temperature_f)
-    upper = WeatherCondition.maximum(:max_temperature_f)
-    lower = lower / 10 * 10
-    upper = upper / 10 * 10
-    (lower..upper).to_a.find_all{ |num| num % incrementer == 0 }
+  def self.ride_metrics(rides)
+    metrics = {}
+    metrics[:average_rides] = self.average_rides(rides)
+    metrics[:lowest_rides] = self.lowest_rides(rides)
+    metrics[:highest_rides] = self.highest_rides(rides)
+    metrics
+  end
+
+  def self.metric_range_with_increments_of(method, incrementer)
+    lower = WeatherCondition.minimum(method.to_sym)
+    upper = WeatherCondition.maximum(method.to_sym)
+    lower = lower / incrementer * incrementer
+    upper = upper / incrementer * incrementer
+    (lower..upper).step(incrementer).to_a
   end
 
   def self.weather_on_day_with_highest_rides
