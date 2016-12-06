2ab207a8d89b4b6fba958f9ba899ac5d289885a1
Mike Schutte <tmikeschutte@gmail.com>
2016-12-06 13:08:38 -0700

    udpate schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161206123136) do
-=======
 ActiveRecord::Schema.define(version: 20161206182531) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
