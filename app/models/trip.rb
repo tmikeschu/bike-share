0c653985cfe15ec64339491e9da75b1b92c2713f
Mike Schutte <tmikeschutte@gmail.com>
2016-12-06 11:05:24 -0700

    update trip association

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ActiveRecord::Base
 
   validates :duration,
             :start_date,
-          :start_station_id,
+            :start_station_id,
             :end_date,
             :end_station_id,
             :bike_id,
@@ -10,7 +10,7 @@ class Trip < ActiveRecord::Base
             :user_zip_code,
             presence: true
 
-  belongs_to :station, :foreign_key => :id
+  belongs_to :station, :foreign_key => :start_station_id
   belongs_to :subscription_type
   belongs_to :weather_condition, :foreign_key => :start_date
 
