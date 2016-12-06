cc8e1d19cf23c2d5d1787fa415e37888c814b7a5
Caroline Powell <carpowell10@gmail.com>
2016-12-06 15:49:49 -0700

    Add pseudocode for subscription_type_spec.rb

---
+++ spec/models/subscription_type_spec.rb
@@ -0,0 +1,5 @@
+#test presence of all data fields
+#test that SubscriptionType.first returns a SubscriptionType
+#test that SubscriptionType.first returns the SubscriptionType we expect
+#test that SubscriptionType.first.trips returns some Trips
+#test that SubscriptionType.first.trips returns the Trips we expect
