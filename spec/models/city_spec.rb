29a926571a41349f1309a2493dfbdec69bfe881d
Caroline Powell <carpowell10@gmail.com>
2016-12-06 15:52:27 -0700

    Add pseudocode for city_spec.rb

---
+++ spec/models/city_spec.rb
@@ -0,0 +1,5 @@
+#test presence of all data fields
+#test that City.first returns a City
+#test that City.first returns the City we expect
+#test that City.first.stations returns some Stations
+#test that City.first.stations returns the Stations we expect
