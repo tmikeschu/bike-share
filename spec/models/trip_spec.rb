55848f4232798fd94d26e92d5bcb7bdc6beac201
Caroline Powell <carpowell10@gmail.com>
2016-12-06 15:46:47 -0700

    Add pseudocode for trip_spec.rb

---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,11 @@
+#test presence of all data fields
+#test that Trip.first returns a Trip
+#test that Trip.first returns the Trip we expect
+#test that Trip.first.weather_condition returns a WC
+#test that Trip.first.weather_condition returns the WC we expect
+#test that Trip.first.subscription_type returns a ST
+#test that Trip.first.subscription_type returns the ST we expect
+#test that Trip.first.start_station returns a ST
+#test that Trip.first.start_station returns the ST we expect
+#test that Trip.first.end_station returns a ST
+#test that Trip.first.end_station returns the ST we expect
