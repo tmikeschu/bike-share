1ac78bb3542533a0de5f03214a4e71981cd63f7a
Mike Schutte <tmikeschutte@gmail.com>
2016-12-06 05:33:16 -0700

    remove added trips_count column from conditions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205224728) do
+ActiveRecord::Schema.define(version: 20161206123136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
