86f6ee1a47e5407ca4f61e46c3b38e4da24835ca
Caroline Powell <carpowell10@gmail.com>
2016-12-06 16:53:26 -0700

    Start validation tests for trips

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,16 +1,204 @@
-#test presence of all data fields
+require_relative '../spec_helper'
 
-#test that Trip.first returns a Trip
-#test that Trip.first returns the Trip we expect
+describe 'Trip' do
+  before do
+    station1 = Station.create !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    station2 = Station.create !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    subscription = SubscriptionType.create !!!!!!!!!!!!!!!!!
+  end
 
-#test that Trip.first.weather_condition returns a WeatherCondition
-#test that Trip.first.weather_condition returns the WeatherCondition we expect
+  describe 'validates' do
+    it 'presence of duration' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
 
-#test that Trip.first.subscription_type returns a SubscriptionType
-#test that Trip.first.subscription_type returns the SubscriptionType we expect
+    it 'presence of start_date' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
 
-#test that Trip.first.start_station returns a Station
-#test that Trip.first.start_station returns the Station we expect
+    it 'presence of start_station_id' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
 
-#test that Trip.first.end_station returns a Station
-#test that Trip.first.end_station returns the Station we expect
+    it 'presence of end_date' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of end_station_id' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of bike_id' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of subscription_type_id' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of user_zip_code' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of created_at' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of updated_at' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of start_date' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+
+    it 'presence of end_time' do
+      invalid_trip = subscription.Trip.create( duration: "1147",
+                                  start_date: "2016/12/5",
+                                  start_station_id: "1",
+                                  end_date: "2016/12/6",
+                                  end_station_id: "2",
+                                  bike_id: "1",
+                                  user_zip_code: "90210",
+                                  created_at: "2016-12-05 22:53:08",
+                                  updated_at: "2016-12-05 22:53:08",
+                                  start_time: "22:53:02",
+                                  end_time: "22:53:08"
+                                  )
+      expect(invalid_trip).to be_invalid
+    end
+  end
+
+end
