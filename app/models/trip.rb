88f79b0bb76bb47334a7193f39d40674d26e3821
Molly Brown <mollbrown@gmail.com>
2016-12-07 12:37:28 -0700

    Refactors station-trip methods

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -29,40 +29,25 @@ class Trip < ActiveRecord::Base
   end
 
   def self.station_with_maximum_rides_as_start_location(station)
-    starts = Trip.where(start_station_id: station)
-    stations = starts.pluck(:end_station_id)
-    end_stations_hash = stations.each_with_object(Hash.new(0)) { |station,counts| counts[station] += 1 }
-    most_frequent_destination_station_id = end_stations_hash.key(end_stations_hash.values.max)
+    most_frequent_destination_station_id = Trip.where(start_station_id: station).group(:end_station_id).count("id").max_by{|bike, count| count }
     Station.where(id: most_frequent_destination_station_id).pluck(:name)
   end
 
   def self.station_with_maximum_rides_as_end_location(station)
-    ends = Trip.where(end_station_id: station)
-    stations = ends.pluck(:start_station_id)
-    start_stations_hash = stations.each_with_object(Hash.new(0)) { |station,counts| counts[station] += 1 }
-    most_frequent_origin_station_id = start_stations_hash.key(start_stations_hash.values.max)
+    most_frequent_origin_station_id = Trip.where(start_station_id: station).group(:start_station_id).count("id").max_by{|bike, count| count }
     Station.where(id: most_frequent_origin_station_id).pluck(:name)
   end
 
   def self.date_with_highest_number_trips(station)
-    start = Trip.where(start_station_id: station)
-    start_dates = start.pluck(:start_date)
-    start_dates_hash = start_dates.each_with_object(Hash.new(0)) { |date,counts| counts[date] += 1 }
-    most_frequent_start_date = start_dates_hash.key(start_dates_hash.values.max)
+    Trip.where(start_station_id: station).group(:start_date).count("id").max_by{|date, count| count }
   end
 
   def self.most_frequent_user_zip_code(station)
-    start = Trip.where(start_station_id: station)
-    zip_codes = start.pluck(:user_zip_code)
-    zip_codes_hash = zip_codes.each_with_object(Hash.new(0)) { |zip_code,counts| counts[zip_code] += 1 }
-    most_frequent_zip_code = zip_codes_hash.key(zip_codes_hash.values.max)
+    Trip.where(start_station_id: station).group(:user_zip_code).count("id").max_by{|zip_code, count| count }
   end
 
   def self.most_frequent_bike_id(station)
-    start = Trip.where(start_station_id: station)
-    bike_ids = start.pluck(:bike_id)
-    bike_ids_hash = bike_ids.each_with_object(Hash.new(0)) { |bike_id,counts| counts[bike_id] += 1 }
-    most_frequent_bike_id = bike_ids_hash.key(bike_ids_hash.values.max)
+    Trip.where(start_station_id: station).group(:bike_id).count("id").max_by{|bike, count| count }
   end
 
   ### Trips Dashboard methods ###
@@ -76,6 +61,20 @@ class Trip < ActiveRecord::Base
 
   def self.minimum_trip_duration
     Trip.minimum(:duration).to_i/60
+  end
+
+    # starts = Trip.where(start_station_id: station)
+    # stations = starts.pluck(:end_station_id)
+    # end_stations_hash = stations.each_with_object(Hash.new(0)) { |station,counts| counts[station] += 1 }
+    # most_frequent_destination_station_id = end_stations_hash.key(end_stations_hash.values.max)
+    # Station.where(id: most_frequent_destination_station_id).pluck(:name)
+
+  def self.station_with_most_rides_as_start_point
+    starts = Trip.joins(:start_station)
+    ids = start_trips.pluck(:start_station_id)
+    ids_hash = ids.each_with_object(Hash.new(0)) { |id,counts| counts[id] += 1 }
+    most_common_start_station_id = ids_hash.key(ids_hash.values.max)
+    Station.where(id: most_common_start_station_id).pluck(:name)
   end
 
 end
