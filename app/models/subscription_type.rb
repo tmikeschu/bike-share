5c54b8bb996721934e316d3c7cd471f2cb637272
Caroline Powell <carpowell10@gmail.com>
2016-12-07 10:00:20 -0700

    Add uniqueness validation and test for subscription_type

--- app/models/subscription_type.rb
+++ app/models/subscription_type.rb
@@ -3,6 +3,8 @@ class SubscriptionType < ActiveRecord::Base
   validates :subscription_type,
             presence: true
 
+  validates :subscription_type, uniqueness: true
+
   has_many :trips
 
 end
