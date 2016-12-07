a7817aa377e5c0972fc739a1f0b9c5d067b8c7c6
Caroline Powell <carpowell10@gmail.com>
2016-12-07 09:52:21 -0700

    Add uniqueness validation to city name

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,6 +3,8 @@ class City < ActiveRecord::Base
   validates :name,
             presence: true
 
+  validates :name, uniqueness: true
+
   has_many :stations
   has_many :trips, through: :stations
 
