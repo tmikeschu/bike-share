1b7ccdc167c476529be46a82379d8db61fbd86f1
Mike Schutte <tmikeschutte@gmail.com>
2016-12-07 21:36:01 -0700

    fix station dashboard feature test

--- spec/features/stations/user_can_see_dashboard_of_stations_metrics_spec.rb
+++ spec/features/stations/user_can_see_dashboard_of_stations_metrics_spec.rb
@@ -8,12 +8,7 @@ describe 'When a user visists /stations-dashboard' do
     @station1 = City.first.stations.create(name: "Denver Union Station", dock_count: 7, installation_date: "2013/12/02", lat: 37.330698, long: -121.888979)
     @station2 = City.last.stations.create(name: "Fort Collins Harmony", dock_count: 9, city_id: 2, installation_date: "2013/8/10", lat: 39.330698, long: -120.888979)
     visit('/stations')
-  end
-
-  it 'there are map markers on the page' do
-    expect(page).to have_content "addMarker"
-  end
-    
+  end  
 
   describe "they can see" do
     it "an all stations heading" do
