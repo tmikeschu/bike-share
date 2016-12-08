5ac5da3eb0befd3c9c733dcd6ae0b6d7ed80e813
Mike Schutte <tmikeschutte@gmail.com>
2016-12-08 05:32:07 -0700

    remove redundant station calls in station model

--- app/models/station.rb
+++ app/models/station.rb
@@ -15,35 +15,35 @@ class Station < ActiveRecord::Base
   has_many :arrival_trips, class_name: 'Trip', :foreign_key => :end_station_id
 
   def self.total_station_count
-    Station.count
+    count
   end
 
   def self.average_bikes_per_station
-    Station.average(:dock_count).to_i
+    average(:dock_count).to_i
   end
 
   def self.maximum_bikes_per_station
-    Station.maximum(:dock_count)
+    maximum(:dock_count)
   end
 
   def self.stations_with_maximum_bikes_available
-    Station.where(dock_count:(Station.maximum(:dock_count))).pluck(:name)
+    where(dock_count:(maximum(:dock_count))).pluck(:name)
   end
 
   def self.minimum_bikes_per_station
-    Station.minimum(:dock_count)
+    minimum(:dock_count)
   end
 
   def self.stations_with_minimum_bikes_available
-    Station.where(dock_count:(Station.minimum(:dock_count))).pluck(:name)
+    where(dock_count:(minimum(:dock_count))).pluck(:name)
   end
 
   def self.newest_station
-    Station.order(:installation_date).last.name
+    order(:installation_date).last.name
   end
 
   def self.oldest_station
-    Station.order(:installation_date).first.name
+    order(:installation_date).first.name
   end
 
 end
